ea1c88f43636c4959cfb51920a8f6eadcb1791ab
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-26 20:00:45 +0300

    Implicitly Type Local Variable

--- LINQSession1/Program.cs
+++ LINQSession1/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Iplicity Type local Variable
+            #region Var [Most Recomended]
+
+            //var name = "Hello World";
+
+            ////var x = null; invalid
+
+            //name = null;
+
+            ////name = 123; invalid
+            //int x;
+            ////var y;
+            //var y=5;
+
+            #region Dynamic
+
+            //dynamic data = "Hello World";
+            //data = 123;
+            //data = true;
+            //data = 20.20;
+
+            //dynamic z;
+            //dynamic t = null;
+            
+            //Console.WriteLine(data);
+
+
+            #endregion
+
+
+
+            #endregion
+
+
+            #endregion
         }
     }
 }
