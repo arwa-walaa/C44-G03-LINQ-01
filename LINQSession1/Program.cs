b8846dea1b6d0d42c46f7c87e66c618faed442f1
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-26 20:35:45 +0300

    04 LINQ Intro

--- LINQSession1/Program.cs
+++ LINQSession1/Program.cs
@@ -51,24 +51,34 @@
 
             //Employee employee = new Employee() { id = 1, name = "John", salary = 50000 };
             //object emp1 = new { id = 1, name = "John", salary = 50000 };
-           
-            var emp1 = new { id = 1, Name = "John", salary = 50000 };
-            Console.WriteLine(emp1.GetType().Name);
 
-            var emp2 = new { id = emp1.id, Name = emp1.Name, salary = 60000 };
-            Console.WriteLine(emp2.GetType().Name);
-            var  emp3= new { Id = 10, Name = "Arwa", Salary = 70000 };
+            // var emp1 = new { id = 1, Name = "John", salary = 50000 };
+            // Console.WriteLine(emp1.GetType().Name);
 
-            Console.WriteLine(emp3.GetType().Name);
+            // var emp2 = new { id = emp1.id, Name = emp1.Name, salary = 60000 };
+            // Console.WriteLine(emp2.GetType().Name);
+            // var  emp3= new { Id = 10, Name = "Arwa", Salary = 70000 };
 
-           var emp4= new { Id = 20, Name = "Sara", Salary = 80000 ,Age=25};
-            Console.WriteLine(emp4.GetType().Name);
+            // Console.WriteLine(emp3.GetType().Name);
 
-            Console.WriteLine(emp1);
-            Console.WriteLine(emp2);
-            Console.WriteLine(emp3);
-            Console.WriteLine(emp4);
+            //var emp4= new { Id = 20, Name = "Sara", Salary = 80000 ,Age=25};
+            // Console.WriteLine(emp4.GetType().Name);
 
+            // Console.WriteLine(emp1);
+            // Console.WriteLine(emp2);
+            // Console.WriteLine(emp3);
+            // Console.WriteLine(emp4);
+
+            #endregion
+
+            #region What is  LINQ
+            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> OddNumbesr=  Numbers.Where(n => n % 2 != 0).ToList();
+            foreach (var item in OddNumbesr)
+            {
+                Console.WriteLine(item);
+            }
             #endregion
         }
     }
