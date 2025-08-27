0508589b3291d85e7065ceb622a1cac9d837a083
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-27 19:54:08 +0300

    LINQ - Restriction Operators

--- Assignmet/Program.cs
+++ Assignmet/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region LINQ - Restriction Operators
+
+            #region Find all products that are out of stock.
+
+            //var Result= ListGenerator.ProductsList.Where(p => p.UnitsInStock == 0);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Find all products that are in stock and cost more than 3.00 per unit.
+
+            //var Result = ListGenerator.ProductsList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3.00M);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Returns digits whose name is shorter than their value.
+            String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var Result = Arr.Where((digit, index) => digit.Length < index);
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            #endregion
+
+
         }
     }
 }
